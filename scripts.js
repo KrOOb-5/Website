732255dbbf530b4e6a75e574d5a5f2d23ee07454
João Agostinho <63238478+KrOOb-5@users.noreply.github.com>
2023-07-28 14:12:24 +0100

    Website

--- scripts.js
+++ scripts.js
@@ -1,3 +1,31 @@
-$(document).ready(function()){
-    $()
-}
+
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 100 ) {
+            $(".nav").css("background-color", "#FFFFFF");  
+            $(".nav").addClass('scrolled');
+
+            $("#logo").css("background", "url('images/logo.webp') no-repeat");
+            $(".buttonnav").css("color", "rgba(254, 195, 1, 1)");
+            $(".buttonnav").css("border",  "2px solid rgba(254, 195, 1, 1)");
+           
+        }
+  
+        else{
+            $(".nav").css("background-color", "transparent");
+
+            $(".nav").removeClass('scrolled');
+            
+            $("#logo").css("background", "url('images/logo2.webp') no-repeat");
+            $(".buttonnav").css("color", "white");
+            $(".buttonnav").css("border",  "2px solid white");
+        }
+    })
+  })
+
+
+            
+           
+            
